0754b9ba194ebb1c09a066af82f1f2f36a764ee1
Hamza Dahbani <dahbanihamza1@gmail.com>
2025-03-17 10:05:40 +0000

    test commit

--- src/main/java/ma/enset/hopital/HopitalApplication.java
+++ src/main/java/ma/enset/hopital/HopitalApplication.java
@@ -1,13 +1,28 @@
 package ma.enset.hopital;
 
+import lombok.Data;
+import ma.enset.hopital.entities.Patient;
+import ma.enset.hopital.repository.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Date;
+
 @SpringBootApplication
-public class HopitalApplication {
+public class HopitalApplication implements CommandLineRunner {
+    @Autowired
+    private PatientRepository patientRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(HopitalApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        patientRepository.save(new Patient(null,"Hamza",new Date(),false,20));
+        patientRepository.save(new Patient(null,"Houcine",new Date(),false,201));
+        patientRepository.save(new Patient(null,"Yassine",new Date(),true,120));
+    }
 }
