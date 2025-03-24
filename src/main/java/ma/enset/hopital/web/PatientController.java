e1c470183f905dd8b61c338f5a45a2733117d7f8
Hamza Dahbani <dahbanihamza1@gmail.com>
2025-03-24 09:33:44 +0000

    Partie 1 commit

---
+++ src/main/java/ma/enset/hopital/web/PatientController.java
@@ -0,0 +1,31 @@
+package ma.enset.hopital.web;
+
+import lombok.AllArgsConstructor;
+import ma.enset.hopital.entities.Patient;
+import ma.enset.hopital.repository.PatientRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@AllArgsConstructor
+public class PatientController {
+    private PatientRepository patientRepository;
+    @GetMapping("/user/index")
+    public String index(Model model,
+                        @RequestParam(name = "page", defaultValue = "0") int page,
+                        @RequestParam(name = "size", defaultValue = "4") int size,
+                        @RequestParam(name = "keyword", defaultValue = "") String keyword){
+
+        //Page<Patient> patientsPage = patientRepository.findAll(PageRequest.of(page,size));
+        Page<Patient> patientsPage = patientRepository.findByNomContains((keyword, PageRequest.of(page,size));
+        model.addAttribute("listPatients", patientsPage.getContent());
+        model.addAttribute("pages",new int[patientsPage.getTotalPages()]);
+        model.addAttribute("currentPage", page);
+        model.addAttribute("keyword", keyword);
+        return "patients";
+    }
+}
