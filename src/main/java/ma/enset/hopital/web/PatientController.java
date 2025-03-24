ae94ae2b0e14c89a4dd7d0a9ff8f9e9cde44ae6f
Hamza Dahbani <dahbanihamza1@gmail.com>
2025-03-24 10:37:43 +0000

    creation d'une page template commit

--- src/main/java/ma/enset/hopital/web/PatientController.java
+++ src/main/java/ma/enset/hopital/web/PatientController.java
@@ -14,18 +14,25 @@ import org.springframework.web.bind.annotation.RequestParam;
 @AllArgsConstructor
 public class PatientController {
     private PatientRepository patientRepository;
-    @GetMapping("/user/index")
+    @GetMapping("/index")
     public String index(Model model,
                         @RequestParam(name = "page", defaultValue = "0") int page,
                         @RequestParam(name = "size", defaultValue = "4") int size,
                         @RequestParam(name = "keyword", defaultValue = "") String keyword){
 
         //Page<Patient> patientsPage = patientRepository.findAll(PageRequest.of(page,size));
-        Page<Patient> patientsPage = patientRepository.findByNomContains((keyword, PageRequest.of(page,size));
+        Page<Patient> patientsPage = patientRepository.findByNomContains(keyword, PageRequest.of(page,size));
         model.addAttribute("listPatients", patientsPage.getContent());
         model.addAttribute("pages",new int[patientsPage.getTotalPages()]);
         model.addAttribute("currentPage", page);
         model.addAttribute("keyword", keyword);
         return "patients";
+    }
+
+    @GetMapping("/delete")
+    public String delete(Long id, String keyword, int page){
+        patientRepository.deleteById(id);
+
+        return "redirect:/index?page="+page+"&keyword="+keyword;
     }
 }
